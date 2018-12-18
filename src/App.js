f89062f0584930fc5ff64d9e5dce28963502591f
pi3rrot <4dakto@gmail.com>
2018-12-18 20:11:35 +0900

    Write Compound Components with static method

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React from 'react'
 import { Switch } from './switch'
 
 class Toggle extends React.Component {
+  static On = ({on, children}) => (on ? children : null)
+  static Off = ({on, children}) => (on ? null : children)
+  static Button = ({on, toggle, ...props}) => (
+    <Switch on={on} onClick={toggle} {...props} />
+  )
   state = {on: false}
   toggle = () =>
     this.setState(
-      ({ on }) => ({ on: !on }),
-      () => {
-        this.props.onToggle(this.state.on);
-      },
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
     )
+
   render() {
-    return <Switch on={this.state.on} onClick={this.toggle} />
+    return React.Children.map(this.props.children, childElement => (
+      React.cloneElement(childElement, {
+        on: this.state.on,
+        toggle: this.toggle,
+      })
+    ))
   }
 }
 const onToggle = (...args) => console.log('onToggle', ...args)
 const Usage = () => {
-  return <Toggle onToggle={onToggle}/>
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The butoon is on</Toggle.On>
+      <Toggle.Off>The butoon is off</Toggle.Off>
+      <Toggle.Button />
+    </Toggle>
+  )
 }
-
-Usage.title = 'Build Toggle'
 
 export { Toggle, Usage as default }
