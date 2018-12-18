cbd9c33931e7845578acae05a0290c58bb7faf20
pi3rrot <4dakto@gmail.com>
2018-12-18 21:30:40 +0900

    Use Render Props

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React from 'react'
 import { Switch } from './switch'
 
-const ToggleContext = React.createContext();
-
-function ToggleConsumer(props) {
-  return (
-    <ToggleContext.Consumer {...props}>
-      {context => {
-        if (!context) {
-          throw new Error(
-            `Toggle compound components cannot be rendered outside the Toggle component`,
-          )
-        }
-        return props.children(context)
-      }}
-    </ToggleContext.Consumer>
-  )
-}
-
 class Toggle extends React.Component {
-  static On = ({children}) => (
-    <ToggleConsumer>{contextValue => (contextValue.on ? children : null)}</ToggleConsumer>
-  )
-  static Off = ({children}) => (
-    <ToggleConsumer>{contextValue => (contextValue.on ? null : children)}</ToggleConsumer>
-  )
-  static Button = (props) => (
-    <ToggleConsumer>{contextValue => (
-      <Switch
-        on={contextValue.on}
-        onClick={contextValue.toggle}
-        {...props}
-      />
-    )}
-    </ToggleConsumer>
-  )
+  state = { on: false }
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
-  state = {on: false, toggle: this.toggle }
   render() {
-    return (
-      <ToggleContext.Provider value={this.state}>
-        {this.props.children}
-      </ToggleContext.Provider>
-    )
+    return this.props.children({
+      on: this.state.on,
+      toggle: this.toggle
+    });
   }
 }
-const onToggle = (...args) => console.log('onToggle', ...args)
-const Usage = () => {
+
+function Usage({
+                 onToggle = (...args) => console.log('onToggle', ...args),
+               }) {
   return (
     <Toggle onToggle={onToggle}>
-      <Toggle.On>The butoon is on</Toggle.On>
-      <Toggle.Off>The butoon is off</Toggle.Off>
-      <div>
-        <Toggle.Button />
-      </div>
+      {({on, toggle}) => (
+        <div>
+          {on ? 'The button is on' : 'The button is off'}
+          <Switch on={on} onClick={toggle} />
+          <hr />
+          <button aria-label="custom-button" onClick={toggle}>
+            {on ? 'on' : 'off'}
+          </button>
+        </div>
+      )}
     </Toggle>
   )
 }
