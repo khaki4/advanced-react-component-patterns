97a68008a19f31033af6781cf073aadd6afc9f61
pi3rrot <4dakto@gmail.com>
2018-12-20 19:59:30 +0900

    Make Controlled React Components with Control Props

--- src/App.js
+++ src/App.js
@@ -4,127 +4,53 @@ import { Switch } from './switch'
 const callAll = (...fns) => (...args) => fns.map(fn => fn && fn(...args));
 
 class Toggle extends React.Component {
-  static defaultProps = {
-    initialOn: false,
-    onReset: () => {},
-    stateReducer: (state, changes) => changes,
+  state = {on: false}
+  isControlled(prop) {
+    return this.props[prop] !== undefined
   }
-  // 💰 any time I use a string as an identifier for a type,
-  // I prefer to give it a variable name. That way folks who
-  // want to reference the type can do so using variable which
-  // will help mitigate the problems of indirection.
-  static stateChangeTypes = {
-    reset: '__toggle_reset__',
-    toggle: '__toggle_toggle__',
-  }
-  initialState = {on: this.props.initialOn}
-  state = this.initialState
-  internalSetState(changes, callback) {
-    this.setState(state => {
-      // handle function setState call
-      const changesObject =
-        typeof changes === 'function' ? changes(state) : changes
-
-      // apply state reducer
-      const reducedChanges =
-        this.props.stateReducer(state, changesObject) || {}
-
-      // remove the type so it's not set into state
-      const {type: ignoredType, ...onlyChanges} = reducedChanges
-
-      // return null if there are no changes to be made
-      return Object.keys(onlyChanges).length ? onlyChanges : null
-    }, callback)
-  }
-
-  reset = () =>
-    this.internalSetState(
-      {...this.initialState, type: Toggle.stateChangeTypes.reset},
-      () => this.props.onReset(this.state.on),
-    )
-  toggle = ({type = Toggle.stateChangeTypes.toggle} = {}) =>
-    this.internalSetState(
-      ({on}) => ({type, on: !on}),
-      () => this.props.onToggle(this.state.on),
-    )
-  getTogglerProps = ({onClick, ...props} = {}) => ({
-    onClick: callAll(onClick, () => this.toggle()),
-    'aria-pressed': this.state.on,
-    ...props,
-  })
-  getStateAndHelpers() {
+  getState() {
     return {
-      on: this.state.on,
-      toggle: this.toggle,
-      reset: this.reset,
-      getTogglerProps: this.getTogglerProps,
+      on: this.isControlled('on') ? this.props.on : this.state.on,
+    }
+  }
+  toggle = () => {
+    if (this.isControlled('on')) {
+      this.props.onToggle(!this.getState().on)
+    } else {
+      this.setState(
+        ({on}) => ({on: !on}),
+        () => {
+          this.props.onToggle(this.getState().on)
+        },
+      )
     }
   }
   render() {
-    return this.props.children(this.getStateAndHelpers())
+    return <Switch on={this.getState().on} onClick={this.toggle} />
   }
 }
 
 class Usage extends React.Component {
-  static defaultProps = {
-    onToggle: (...args) => console.log('onToggle', ...args),
-    onReset: (...args) => console.log('onReset', ...args),
-  }
-  initialState = {timesClicked: 0}
-  state = this.initialState
-  handleToggle = (...args) => {
-    this.setState(({timesClicked}) => ({
-      timesClicked: timesClicked + 1,
-    }))
-    this.props.onToggle(...args)
-  }
-  handleReset = (...args) => {
-    this.setState(this.initialState)
-    this.props.onReset(...args)
-  }
-  toggleStateReducer = (state, changes) => {
-    if (changes.type === 'forced') {
-      return changes
-    }
-    if (this.state.timesClicked >= 4) {
-      return {...changes, on: false}
-    }
-    return changes
+  state = {bothOn: false}
+  handleToggle = on => {
+    this.setState({bothOn: on})
   }
   render() {
-    const {timesClicked} = this.state
+    const {bothOn} = this.state
+    const {toggle1Ref, toggle2Ref} = this.props
     return (
-      <Toggle
-        stateReducer={this.toggleStateReducer}
-        onToggle={this.handleToggle}
-        onReset={this.handleReset}
-        ref={this.props.toggleRef}
-      >
-        {({on, toggle, reset, getTogglerProps}) => (
-          <div>
-            <Switch
-              {...getTogglerProps({
-                on: on,
-              })}
-            />
-            {timesClicked > 4 ? (
-              <div data-testid="notice">
-                Whoa, you clicked too much!
-                <br />
-                <button onClick={() => toggle({type: 'forced'})}>
-                  Force Toggle
-                </button>
-                <br />
-              </div>
-            ) : timesClicked > 0 ? (
-              <div data-testid="click-count">
-                Click count: {timesClicked}
-              </div>
-            ) : null}
-            <button onClick={reset}>Reset</button>
-          </div>
-        )}
-      </Toggle>
+      <div>
+        <Toggle
+          on={bothOn}
+          onToggle={this.handleToggle}
+          ref={toggle1Ref}
+        />
+        <Toggle
+          on={bothOn}
+          onToggle={this.handleToggle}
+          ref={toggle2Ref}
+        />
+      </div>
     )
   }
 }
